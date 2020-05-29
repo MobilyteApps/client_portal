cf5c72370c8f8e349519d18282db4c70b36994c7
Stephen Cozart <stephen@creativeanvil.com>
2020-05-29 13:50:58 -0500

    add logo to mobile login form

--- lib/src/widgets/login_form_container.dart
+++ lib/src/widgets/login_form_container.dart
@@ -8,6 +8,50 @@ class LoginFormContainer extends StatelessWidget {
 
   LoginFormContainer(this.userModel);
 
+  Widget _logo(context) {
+    if (MediaQuery.of(context).size.width > 768) {
+      return SizedBox();
+    }
+    return Container(
+      width: 330,
+      margin: EdgeInsets.only(bottom: 15),
+      child: Image.asset(
+        'images/logo.png',
+        alignment: Alignment.centerLeft,
+        width: 150,
+      ),
+      alignment: Alignment.centerLeft,
+    );
+  }
+
+  Widget _welcomeText(context) {
+    var fontSize = 24.0;
+    var text = 'Welcome';
+    var fontWeight = FontWeight.w500;
+    var color = Color.fromRGBO(61, 61, 61, 1);
+    var margin = 0.0;
+
+    if (MediaQuery.of(context).size.width <= 768) {
+      fontSize = 14.0;
+      color = Color.fromRGBO(117, 117, 117, 1);
+      text = "Sign in to continue";
+      margin = 10.0;
+    }
+
+    return Container(
+      margin: EdgeInsets.only(bottom: margin),
+      child: Text(
+        text,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,16 +60,10 @@ class LoginFormContainer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          _logo(context),
           SizedBox(
             width: 330,
-            child: Text(
-              'Welcome',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                  color: Color.fromRGBO(61, 61, 61, 1)),
-            ),
+            child: _welcomeText(context),
           ),
           SizedBox(
             width: 360,

e614d82691a6975a1843f013fbe320b18a9e1f9d
Stephen Cozart <stephen@creativeanvil.com>
2020-06-24 12:52:49 -0500

    moved full schedule view

--- lib/src/views/ScheduleView.dart
+++ lib/src/views/ScheduleView.dart
@@ -72,27 +72,6 @@ class _ScheduleViewState extends State<ScheduleView> {
       });
     });
 
-    if (entries.length > 0) {
-      entries.add(
-        Container(
-          alignment: Alignment.centerRight,
-          child: FlatButton(
-            hoverColor: Colors.transparent,
-            padding: EdgeInsets.all(0),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/calendar');
-            },
-            child: Text(
-              'VIEW FULL SCHEDULE',
-              style: TextStyle(
-                color: Brand.primary,
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
     return entries;
   }
 
@@ -118,6 +97,40 @@ class _ScheduleViewState extends State<ScheduleView> {
               http.Response response = snapshot.data;
               if (response.statusCode == 200 || response.statusCode == 201) {
                 var body = json.decode(response.body.toString());
+                var _entries = entries(body, context);
+                Widget _viewFullSchedule = Container(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    hoverColor: Colors.transparent,
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/calendar');
+                    },
+                    child: Text(
+                      'VIEW FULL SCHEDULE',
+                      style: TextStyle(
+                        color: Brand.primary,
+                      ),
+                    ),
+                  ),
+                );
+                if (_entries.isEmpty) {
+                  _viewFullSchedule = SizedBox();
+                }
+                var _header = Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Upcoming Dates',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    _viewFullSchedule,
+                  ],
+                );
                 return Container(
                   constraints: BoxConstraints(maxWidth: 480),
                   padding: _padding,
@@ -125,13 +138,7 @@ class _ScheduleViewState extends State<ScheduleView> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Container(
-                        child: Text(
-                          'Upcoming Dates',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
+                        child: _header,
                         margin: EdgeInsets.only(bottom: 25),
                       ),
                       Expanded(
@@ -139,10 +146,10 @@ class _ScheduleViewState extends State<ScheduleView> {
                           controller: _scrollController,
                           isAlwaysShown: true,
                           child: ListView(
-                            padding: EdgeInsets.only(right: 15),
+                            padding: EdgeInsets.only(right: 15, bottom: 15),
                             controller: _scrollController,
                             shrinkWrap: true,
-                            children: entries(body, context),
+                            children: _entries,
                           ),
                         ),
                       ),

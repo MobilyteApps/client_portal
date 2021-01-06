98aaa7347d3fc405b8beaf86835b01f42098b10a
Stephen Cozart <stephen@creativeanvil.com>
2021-01-06 13:02:57 -0600

    version bump

--- lib/src/widgets/Layout.dart
+++ lib/src/widgets/Layout.dart
@@ -18,7 +18,7 @@ class Layout extends StatefulWidget {
 
   final Widget content;
 
-  final String version = '0.0.13';
+  final String version = '0.0.14';
 
   Layout({this.model, this.content});
 
